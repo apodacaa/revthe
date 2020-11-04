8885a5a83d1a6aa513a30f690d12189935638256
Anthony Apodaca <anthonyapodaca@mac-mini.lan>
2020-11-04 08:50:35 -0500

    removed contact

--- pages/about.vue
+++ pages/about.vue
@@ -5,8 +5,6 @@
     <p>Revelry Theater is a small performing arts studio primarily used for classes and training in the areas of acting, improvisation, comedy, directing, and production. We also offer weekend showcases to foster the growth and education of our students and community.</p>
     <p>The main purpose of Revelry Theater is to assist individuals in becoming powerful and fearless performers, producers, and directors. We strive to remove any economic barriers to participation by keeping our classes, workshops, rentals, and shows as affordable as possible.</p>
     <p>Revelry Theater Inc is a non-profit 501(c)(3) corporation. Our board and staff is 100% volunteer.</p>
-    <h2 class="title is-2">Contact</h2>
-    <p>contact@revlerytheater.com</p>
     <div class="links">
         <NuxtLink
           class="button is-outlined is-primary"
